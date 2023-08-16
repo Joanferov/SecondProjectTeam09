c218fe6d8426a01f3aaec1d651adbdbeb19031f1
Joanferov <jonathanfernandez777@gmail.com>
2023-08-15 22:56:14 -0500

    FinalMovesMario

--- Assets/Scripts/PlayerSpriteRenderer.cs
+++ Assets/Scripts/PlayerSpriteRenderer.cs
@@ -9,7 +9,7 @@ public class PlayerSpriteRenderer : MonoBehaviour
     public Sprite idle;
     public Sprite jump;
     public Sprite slide;
-    public Sprite run;
+    public AnimatedSprite run;
 
     private void Awake()
     {
@@ -17,18 +17,29 @@ public class PlayerSpriteRenderer : MonoBehaviour
         movement = GetComponentInParent<PlayerMovement>();
     }
 
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+
+
     private void LateUpdate()
         {
+        run.enabled = movement.running;
+
         if (movement.jumping)
         {
             spriteRenderer.sprite = jump;
         } else if (movement.sliding)
         {
             spriteRenderer.sprite = slide;
-        } else if (movement.running)
-        {
-            spriteRenderer.sprite = run;
-        } else
+        } 
+         else if (!movement.running)
         {
             spriteRenderer.sprite = idle;
         }
