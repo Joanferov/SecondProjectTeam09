e5aeba0c7bedcccc3b2e82d43803c5c839a8a5ac
Joanferov <jonathanfernandez777@gmail.com>
2023-08-16 23:33:46 -0500

    enemigo Goomba

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+public static GameManager Instance { get; private set; }
+
+    public int world { get; private set; }
+    public int stage { get; private set; }
+    public int lives { get; private set; }
+
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        } else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        lives = 3;
+
+        LoadLevel(1, 1);
+    }
+
+    private void LoadLevel(int world, int stage)
+    {
+        this.world = world;
+        this.stage = stage;
+
+        SceneManager.LoadScene($"{world}-{stage}");
+    }
+
+    public void NextLevel()
+    {
+        LoadLevel(world, stage + 1);
+    }
+
+
+    public void ResetLevel(float delay)
+
+    {
+        Invoke(nameof(ResetLevel), delay);
+    }
+
+
+    public void ResetLevel()
+    {
+        lives--;
+
+        if (lives > 0)
+        {
+            LoadLevel(world, stage);
+        } else
+        {
+            GameOver();
+        }
+
+    }
+
+    public void GameOver()
+    {
+        NewGame();
     }
 }
